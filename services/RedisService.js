f35b66be3ab18ef83c8fb612e6006ce201241c48
lglong519 <769141564@qq.com>
2018-09-03 18:42:26 +0800

    RedisService

---
+++ services/RedisService.js
@@ -0,0 +1,53 @@
+const Redis = require('ioredis');
+const Joi = require('joi');
+const RandToken = require('rand-token');
+const _ = require('lodash');
+const nconf = require('nconf');
+
+const client = new Redis(nconf.get('REDIS_URI'));
+
+/**
+ * @description
+ * @param {Object} values
+ * @param {String} [id] 唯一凭证，默认:随机生成
+ * @param {String} [return] 返回的凭证名称，默认：id
+ * @param {Object} options 选项
+ * @param {Object} type 类型：[default,a-z,alpha,A-Z,ALPHA,0-9,numeric,base32]
+ * @param {Object} length 长度，默认：9
+ * @return {Object} {id:PQ78OmsAsz,num: 9527} or {key:PQ78OmsAsz,num: 9527}
+ */
+const create = (values, options) => {
+	const schema = Joi.object().min(1).required();
+	const defaultOptions = {
+		type: 'default',
+		length: 9,
+		expire: nconf.get('REDIS_EXPIRE_SECONDS') || 10
+	};
+	options = Object.assign(defaultOptions, options);
+	const rand = RandToken.generator({
+		chars: options.type
+	});
+
+	if (!values.id) {
+		values.id = rand.generate(options.length);
+	}
+	let doc = _.omit(values, ['id', 'return']);
+	if (values.return) {
+		doc[values.return] = values.id;
+	} else {
+		doc.id = values.id;
+	}
+	schema.validate(doc, (err, value) => {
+		if (err) {
+			throw err;
+		}
+	});
+	return client.multi().hmset(values.id, doc).expire(values.id, options.expire).hgetall(values.id).exec().then(result => {
+		return _.get(result, '[2][1]');
+	}).catch(err => {
+		console.error('RedisService create', err);
+	});
+};
+module.exports = {
+	create
+};
