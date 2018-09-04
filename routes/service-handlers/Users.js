d0d000bd0def3af28f754587b3f690ab7a02f297
lglong519 <769141564@qq.com>
2018-09-04 19:18:26 +0800

    service-api: users

---
+++ routes/service-handlers/Users.js
@@ -0,0 +1,27 @@
+const restifyMongoose = require('common/restify-mongoose');
+const Joi = require('joi');
+const regExp = require('common/regExp');
+const Errors = require('restify-errors');
+
+const handler = restifyMongoose('User');
+const beforeSave = (req, model, cb) => {
+	const schema = Joi.object().keys({
+		username: Joi.string().regex(regExp.account).required(),
+		email: Joi.string().email().required(),
+		phone: Joi.string().regex(regExp.CHNPhone),
+		password: Joi.string().regex(regExp.password).required(),
+		client: Joi.string().required().required(),
+	}).unknown().required();
+	const validate = Joi.validate(model, schema);
+	if (validate.error) {
+		return cb(new Errors.InvalidArgumentError(validate.error));
+	}
+	cb();
+};
+module.exports = {
+	insert: handler.insert({ beforeSave }),
+	query: handler.query(),
+	detail: handler.detail(),
+	update: handler.update(),
+	delete: handler.delete(),
+};
--- routes/service.js
+++ routes/service.js
@@ -8,6 +8,15 @@ const router = new RestifyRouter();
 router.post(SERVICES_API + 'accesses', handlers.Accesses.insert);
 router.post(SERVICES_API + 'access-tokens', handlers.AccessTokens.create);
 
+const authRouter = new RestifyRouter();
+
+authRouter.post(SERVICES_API + 'users', handlers.Users.insert);
+authRouter.get(SERVICES_API + 'users', handlers.Users.query);
+authRouter.get(SERVICES_API + 'users/:id', handlers.Users.detail);
+authRouter.patch(SERVICES_API + 'users/:id', handlers.Users.update);
+authRouter.del(SERVICES_API + 'users/:id', handlers.Users.delete);
+
 module.exports = server => {
 	router.applyRoutes(server);
+	authRouter.applyRoutes(server);
 };
